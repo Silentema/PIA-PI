0c2adea34fea18cbe31caa172a8c5140fdd4f6c3
Silentema <eric.lopezp@uanl.edu.mx>
2026-09-03 23:09:05 -0600

    Nuevas cositas, aun tengo que cambiar el boton para las gorras

--- js/javascript.js
+++ js/javascript.js
@@ -22,3 +22,48 @@ fetch("componentes/header.html")
 
 
   //Aparir de aqui voy a poner todos las demas fuciones de JS, de preferencia solo hay que usar un Archivo JS para mas simplicidad 
+const openBtn = document.getElementById("Open-Modal");
+const closeBtn = document.getElementById("Close-Modal");
+const modal = document.getElementById("Modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("Open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("Open");
+});
+
+
+
+
+const modelos = [
+    "assets/GorraPiratas.glb",
+    "assets/GorraTigres.glb",
+    "assets/GorraPericos.glb",
+    "assets/GorraOlmecas.glb",
+    "assets/GorraLeones.glb",
+    "assets/GorraGuerreros.glb",
+    "assets/GorraElAguila.glb",
+    "assets/GorraDiablos.glb",
+    "assets/GorraConspiradores.glb",
+    "assets/GorraBravos.glb",
+    "assets/Silla-de-angel-para-AR.glb",
+    "assets/resetVectorsv2.glb",
+];
+
+let modeloActual = 0;
+
+function cambiarModelo() {
+    const modelViewer = document.getElementById("miModelo");
+
+    let nuevoModelo;
+
+    do {
+        nuevoModelo = Math.floor(Math.random() * modelos.length);
+    } while (nuevoModelo === modeloActual && modelos.length > 1);
+
+    modeloActual = nuevoModelo;
+
+    modelViewer.src = modelos[modeloActual];
+}
